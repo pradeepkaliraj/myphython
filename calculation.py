f2a8306f826604ff22c9ad25375f2f59cb8cb697
pradeepkaliraj <99564343+pradeepkaliraj@users.noreply.github.com>
2022-02-24 18:33:31 +0100

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 31 19:45:39 2022
+
+@author: kalirajp
+"""
+
+def summation(a,b):
+    return a+b
+def multiplication(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+    
